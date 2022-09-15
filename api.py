6797ea8cbebe08e58e4e721fc9f0991d19a4e3ed
Nitish Vantha <nitish@Nitishs-MacBook-Pro-2.local>
2022-09-15 03:03:12 -0400

    Read from directory

--- api.py
+++ api.py
@@ -75,11 +75,10 @@ def analyzeWrapper(req, trx_id = 0):
     resp_obj = jsonify({'success': False})
 
     instances = []
-    if "img" in list(req.keys()):
-        raw_content = req["img"] #list
 
-        for item in raw_content: #item is in type of dict
-            instances.append(item)
+    directory = 'database/analyze'
+    for filename in os.listdir(directory):
+        instances.append(f"{directory}/{filename}")
 
     if len(instances) == 0:
         return jsonify({'success': False, 'error': 'you must pass at least one img object in your request'}), 205
@@ -90,7 +89,7 @@ def analyzeWrapper(req, trx_id = 0):
 
     detector_backend = 'opencv'
 
-    actions= ['emotion', 'age', 'gender', 'race']
+    actions= ['age', 'gender']
 
     if "actions" in list(req.keys()):
         actions = req["actions"]
@@ -101,7 +100,7 @@ def analyzeWrapper(req, trx_id = 0):
     #---------------------------
 
     try:
-        resp_obj = DeepFace.analyze(instances, actions = actions)
+        resp_obj = DeepFace.analyze(instances, actions = actions, enforce_detection=False)
     except Exception as err:
         print("Exception: ", str(err))
         return jsonify({'success': False, 'error': str(err)}), 205
@@ -151,26 +150,13 @@ def verifyWrapper(req, trx_id = 0):
     #----------------------
 
     instances = []
-    if "img" in list(req.keys()):
-        raw_content = req["img"] #list
 
-        for item in raw_content: #item is in type of dict
-            instance = []
-            img1 = item["img1"]; img2 = item["img2"]
-
-            validate_img1 = False
-            if len(img1) > 11 and img1[0:11] == "data:image/":
-                validate_img1 = True
-
-            validate_img2 = False
-            if len(img2) > 11 and img2[0:11] == "data:image/":
-                validate_img2 = True
-
-            if validate_img1 != True or validate_img2 != True:
-                return jsonify({'success': False, 'error': 'you must pass both img1 and img2 as base64 encoded string'}), 205
-
-            instance.append(img1); instance.append(img2)
-            instances.append(instance)
+    directory = 'database/verify'
+    for folder in os.listdir(directory):
+        instance = []
+        instance.append(f"{directory}/{folder}/image1.jpeg")
+        instance.append(f"{directory}/{folder}/image2.jpeg")
+        instances.append(instance)
 
     #--------------------------
 
@@ -186,6 +172,7 @@ def verifyWrapper(req, trx_id = 0):
             , model_name = model_name
             , distance_metric = distance_metric
             , detector_backend = detector_backend
+            , enforce_detection=False
         )
 
         if model_name == "Ensemble": #issue 198.
